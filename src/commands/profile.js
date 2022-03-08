053ee310295cad45181ed4ef58f562364c4111ea
Sarah Little <litsarah+github@gmail.com>
2022-03-08 10:03:33 -0600

    Fix autoformatter

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -1,23 +1,28 @@
-require("dotenv").config();
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { getBungieNetUserById, convertUserResponseToEmbed, searchByGlobalNamePost } = require("../bungienet/endpoints/player.js");
+const {
+  getBungieNetUserById,
+  convertUserResponseToEmbed,
+  searchByGlobalNamePost,
+} = require("../bungienet/endpoints/player.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("profile")
     .setDescription("Replies with Bungie profile info")
-    .addStringOption(option =>
-      option.setName("name")
+    .addStringOption((option) =>
+      option
+        .setName("name")
         .setDescription("The display name of the player")
-        .setRequired(true)),
+        .setRequired(true),
+    ),
 
   async execute(interaction) {
-    const response = await searchByGlobalNamePost(interaction.options.getString("name"));
+    const response = await searchByGlobalNamePost(
+      interaction.options.getString("name"),
+    );
     const id = response[0].bungieNetMembershipId;
     const profile = await getBungieNetUserById(id);
 
-    interaction.reply(
-      { embeds: [convertUserResponseToEmbed(profile)] },
-    );
+    interaction.reply({ embeds: [convertUserResponseToEmbed(profile)] });
   },
 };
