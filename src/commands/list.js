33142369fa0de76649c3b469d94061eaa0e537ec
Sarah Little <litsarah+github@gmail.com>
2022-03-11 02:40:20 -0600

    Add tracking of D2 clans

---
+++ src/commands/list.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { GuildClans } = require("../database");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("list")
+    .setDescription("Replies with a list of clans registered to this server"),
+  async execute(interaction) {
+    const clanList = await GuildClans.findAll({
+      attributes: ["clan"],
+      where: { guild: interaction.guild.id },
+    });
+    const clanString =
+      clanList.map((c) => c.clan).join("\n") || "No clans found";
+
+    return interaction.reply(`List of clans:\n${clanString}`);
+  },
+};
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
+const { GuildClans } = require("../database");
+
 module.exports = {
   name: "ready",
   once: true,
   execute(client) {
+    if (process.env.TEST_ENV == "true") {
+      GuildClans.sync({ force: true });
+    } else {
+      GuildClans.sync();
+    }
     console.log(`Ready! Logged in as ${client.user.tag}`);
   },
 };
